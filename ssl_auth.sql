af11f98ebefe0f4dc54b912c8e04c0aaf815796f
root <root@localhost.(none)>
2013-04-10 15:28:08 +0800

    modified:   common/create.class.php 	modified:   ssl_auth.sql

--- ssl_auth.sql
+++ ssl_auth.sql
@@ -1,6 +1,6 @@
-CREATE TABLE `ssl`.`ssl_auth` (`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+CREATE TABLE `ssl_auth` (`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
 `uid` INT NOT NULL COMMENT '会员id',
-`pfxcontent` TEXT NOT NULL COMMENT '证书内容',
+`pfxcontent` VARCHAR( 200 ) NOT NULL  COMMENT '证书内容',
 `outtime` INT NOT NULL COMMENT '过期时间',
 `create` INT NOT NULL COMMENT '生成时间'
 ) ENGINE = MYISAM COMMENT = '证书安装记录表';
